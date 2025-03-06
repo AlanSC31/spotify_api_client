041e920cbb99788536fde1a804344a6e2f597c70
Aln <alan.sanchez3201@alumnos.udg.mx>
2025-03-06 09:54:52 -0600

    Se cambio la estructura del codigo

--- spotify_api_client/app.py
+++ spotify_api_client/app.py
@@ -1,47 +1,54 @@
-from flask import Flask, request, jsonify
+import json
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-app = Flask(__name__)
-
-#credenciales para conexion con la api
+# Credenciales de la API de Spotify
 app_id = '0b8cd645c7284c1aa9519857fbccd9ca'
 secret_id = 'a8c395d102c74bf9b7915f18ed2dfc8d'
 
-#inicializar api con spotipy con autenticacion
+# Inicializar la API con Spotify y autenticación
 auth_manager = SpotifyClientCredentials(client_id=app_id, client_secret=secret_id)
 sp = spotipy.Spotify(auth_manager=auth_manager)
 
-#home route
-@app.route("/")
-def home():
-    return 'Spotify api client'
-
-#search route
-@app.route('/search', methods=['GET'])
-def artist_top_ten():
-    #se obtiene el artista de los parametros de la consulta, se coloca valor por defecto
-    artist_name = request.args.get('artist', 'Bad Bunny')
-    #busca en el api el artista que se paso en parametros y retorna la respuesta en json
-    result = sp.search(q=artist_name, type='artist')
-
-    # Obtener el ID del primer artista que aparece en los resultados
-    artist_id = result['artists']['items'][0]['id']
-    top_tracks = sp.artist_top_tracks(artist_id)
+def lambda_handler(event, context):
+    # Obtiene el nombre del artista de los parámetros de la consulta 
+    artist_name = event['queryStringParameters'].get('artist', 'Bad Bunny') #valor por defecto
     
-    #extrae los nombres de las canciones y enumerar
-    top_10_tracks = []
-    for idx, track in enumerate(top_tracks['tracks'][:10], start=1):  
-        track_info = {
-            'rank': idx,  
-            'name': track['name'],
+    try:
+        # Busca el artista usando la API de Spotify
+        result = sp.search(q=artist_name, type='artist')
+
+        # Obtener el ID del primer artista en los resultados
+        artist_id = result['artists']['items'][0]['id']
+        top_tracks = sp.artist_top_tracks(artist_id)
+
+        # Extraer los nombres de las canciones y enumerarlas
+        top_10_tracks = []
+        for idx, track in enumerate(top_tracks['tracks'][:10], start=1):
+            track_info = {
+                'rank': idx,
+                'name': track['name'],
+            }
+            top_10_tracks.append(track_info)
+
+        # Responder con el top 10 de canciones en formato JSON
+        response = {
+            'statusCode': 200,
+            'body': json.dumps(top_10_tracks),
+            'headers': {
+                'Content-Type': 'application/json'
+            }
         }
-        # agrega a la lista los resultados
-        top_10_tracks.append(track_info)
-    
-    # Devolver la lista del top 10
-    return jsonify(top_10_tracks)
 
+        return response
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    except Exception as e:
+        response = {
+            'statusCode': 500,
+            'body': json.dumps({'error': str(e)}),
+            'headers': {
+                'Content-Type': 'application/json'
+            }
+        }
+
+        return response
